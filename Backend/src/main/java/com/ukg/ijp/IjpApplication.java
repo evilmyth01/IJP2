31c4bf37580a97965e9def785a28bdb603e16bc1
vishal.goswami <vishalgoswami02018@gmail.com>
2024-08-08 10:44:29 +0530

    initial commit

---
+++ Backend/src/main/java/com/ukg/ijp/IjpApplication.java
@@ -0,0 +1,13 @@
+package com.ukg.ijp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IjpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IjpApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/ukg/ijp/IjpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ukg.ijp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IjpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
